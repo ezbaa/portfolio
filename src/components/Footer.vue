63c9cf5af1816dc09631f2f1e5463069a84a21c6
Essi <124583695+ezbaa@users.noreply.github.com>
2025-10-09 17:01:32 +0300

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <p>© 2025 Essi Antola</p>
+  </footer>
+</template>
+<script setup></script>
+<style>
+footer {
+  text-align: center;
+  font-size: 0.8rem;
+  margin-top: 3rem;
+}
+</style>
