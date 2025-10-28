3cc92860100156d5e56efd9bad16abed9f0d3059
Essi <124583695+ezbaa@users.noreply.github.com>
2025-10-28 10:18:55 +0200

    Move image folder and edit text

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,13 +6,12 @@
 
     <p>
       Hey there! I'm Essi, a developer from Finland. I recently graduated as a
-      Software Developer and completed a six-month internship where I worked on
-      both frontend and backend projects. I enjoy working on the full stack,
-      taking features from concept to deployment, and understanding how code
-      moves from user interfaces to the server and finally to a live
-      environment. Right now, my main focus is growing as a full-stack
-      developer, experimenting with new tools, and taking on projects that
-      challenge me.
+      software developer and completed a six-month internship where I worked on
+      both frontend and backend projects. I enjoy full-stack development,
+      taking features from concept to deployment and understanding how code
+      flows from the user interface to the server and into production.
+      Right now, I’m focused on growing as a full-stack developer, exploring new tools,
+      and building projects that challenge me.
     </p>
   </div>
 </template>
