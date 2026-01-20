8e65655cf8522a0d3d1c458c20b5dfb6b6ff1038
ezbaa <essi.antola@hotmail.com>
2026-01-20 14:22:07 +0200

    Add ProjectCard section

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,9 +1,55 @@
-<script setup></script>
 <template>
-  <div id="projects">
-    <h1>My projects</h1>
-    <p>The projects I have been working on</p>
-  </div>
+  <section id="projects" class="projects">
+    <h2>Projects</h2>
+
+    <div class="projects-grid">
+      <ProjectCard
+        v-for="project in projects"
+        :key="project.id"
+        :title="project.title"
+        :description="project.description"
+        :image="project.image"
+        :techStack="project.techStack"
+        :githubUrl="project.githubUrl"
+      />
+    </div>
+  </section>
 </template>
 
-<style scoped></style>
+<script setup>
+import ProjectCard from "./ProjectCard.vue";
+
+const projects = [
+  {
+    title: "Portfolio Website",
+    description: "My personal porftolio",
+    image: "/portfolio.png",
+    techStack: ["Vue.js"],
+    githubUrl: "https://github.com/ezbaa/portfolio",
+  },
+  {
+    title: "Dev news Slack bot",
+    description:
+      "A bot that posts daily developer news to a Slack channel using Dev.to API. The project uses Python, Slack webhooks, and GitHub Actions for scheduling",
+    image: "/slack_bot.png",
+    techStack: ["Python"],
+    githubUrl: "https://github.com/ezbaa/slack-dev-news-bot",
+  },
+];
+</script>
+
+<style scoped>
+.projects {
+  font-size: 1rem;
+  padding: 2rem;
+  text-align: center;
+}
+
+.projects-grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 2rem;
+  margin-top: 2rem;
+}
+</style>
